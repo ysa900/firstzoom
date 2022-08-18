fdf9a55596498744654acf914ecc3ef55bb689e7
Huiseok Yang <ysa7176@naver.com>
2022-08-18 15:32:38 +0900

    second commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,82 @@
-alert("hi");
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room  = document.getElementById("room");
+
+room.hidden = true;
+let roomName;
+
+function handleMessageSubmit(event){
+
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message",input.value, roomName, () => {
+
+        addMessage(`You:${value}`);
+
+    });
+   input.value="";
+
+}
+
+function handleNicknameSubmit(event){
+
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", input.value);
+
+}
+
+
+function showRoom(){
+
+    welcome.hidden= true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
+}
+
+
+
+function handleRoomSubmit(event){
+
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom );
+    roomName = input.value;
+    input.value="";
+
+}
+
+function addMessage(msg){
+
+    const ul= room.querySelector("ul");
+    const li = document.createElement("li");
+
+    li.innerText = msg;
+    ul.appendChild(li);
+}
+
+
+form.addEventListener("submit",handleRoomSubmit);
+
+socket.on("welcome", (user)=> {
+
+    addMessage(`${user} Joined!`);
+
+});
+
+socket.on("bye", (left)=> {
+
+    addMessage(`${left} left ㅜㅜ!`);
+
+});
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,87 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname +"/public" ));
-app.get("/", (req,res) => res.render("home"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const httpServer= http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+
+wsServer.on("connection", (socket) =>{
+
+    socket["nickname"]="Anone";
+
+    socket.onAny((event) => {
+
+        
+
+        console.log(`Socket Event:${event}`);            
+    });
+
+    socket.on("enter_room", (roomName,done)=>{
+
+       
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname);
+
+    } );
+
+    socket.on("disconnecting", ()=> {
+
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    });
+
+    socket.on("new_message", (msg,room, done)=>{
+
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+
+    })
+
+    socket.on("nickname", (nickname) => socket["nickname"]= nickname);
+
+});
+
+httpServer.listen(3000, handleListen);
+
+
+
+
+/* 
+import WebSocket from "ws";
+
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+wss.on("connection",(socket)=>{
+
+    
+    sockets.push(socket);
+    socket["nickname"]= "Anon";
+    console.log("Connected to Browser");
+    socket.on("close", ()=> console.log("DisConnected from Browser"));
+
+    socket.on("message", (msg)=>{
+           const message = JSON.parse(msg);
+
+            switch (message.type){
+                case "new_message":
+                    sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                    break;
+                case "nickname":
+                    socket["nickname"]= message.payload;
+            }
+
+            
+           
+           
+    })
+   
+
+} );
+ */
